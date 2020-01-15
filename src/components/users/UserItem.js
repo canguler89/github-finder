871d7f162a9e189918d60083e2377e5053a0d36e
Can Muhammet GULER <canmuhammetguler@Cans-MacBook-Pro.local>
2020-01-15 17:13:06 -0600

    users added

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,7 +2,23 @@ import React, { Component } from "react";
 
 class UserItem extends Component {
   render() {
-    return <div>UserItem</div>;
+    const { login, html_url, avatar_url } = this.props.user;
+    return (
+      <div className="card text-center">
+        <img
+          src={avatar_url}
+          alt=""
+          className="round-img"
+          style={{ width: "60px" }}
+        ></img>
+        <h3>{login}</h3>
+        <div>
+          <a href={html_url} className="btn btn-dark btn-sm my-1">
+            More
+          </a>
+        </div>
+      </div>
+    );
   }
 }
 export default UserItem;
